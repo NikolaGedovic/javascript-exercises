0b8bf6bcbdef0f4f070c664278aaa371715161f6
Nikola <gedovicc96@gmail.com>
2023-10-11 09:26:28 +0200

    sum all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
+const sumAll = function (start, end) {
+    let totalSum = 0;
+    for (let i = start; i <= end; i++) {
+        totalSum += i;
+    };
+    return totalSum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
