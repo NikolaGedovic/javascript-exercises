d0a624aebbac0cfe52df9521466131a26cdec0b7
Nikola <gedovicc96@gmail.com>
2023-10-11 10:05:18 +0200

    convert temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const tempCelsius = (temp - 32) * 5 / 9;
+  const roundedCelsius = Math.round(tempCelsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const tempFahrenheit = temp * (9 / 5) + 32;
+  const roundedFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
